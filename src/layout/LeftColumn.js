7824b7c958bda8a799091af763ca2ecf706cf57f
Sandro Bochorishvili <Sanideth@gmail.com>
2021-03-28 18:35:52 +0400

    added layout for markup

---
+++ src/layout/LeftColumn.js
@@ -0,0 +1,28 @@
+import MainLogo from "../components/MainLogo";
+import ButtonCta from "../components/ButtonCta";
+
+const LeftColumn = () => (
+  <div className="left-column">
+    <div className="left-column__logo-container">
+      <a href="https://github.com/Sanideth" className="left-column__logo-link">
+        <MainLogo />
+      </a>
+    </div>
+    <div className="left-column__heading-container">
+      <p className="left-column__heading-intro">
+        <span className="left-column__span">New</span>Monograph Dashboard
+      </p>
+      <h1 className="heading-primary">Powerful Insights Into Your Team</h1>
+      <p className="left-column__heading-middle">
+        Project planning and time tracking <br />
+        for agile teams
+      </p>
+    </div>
+    <div className="left-column__cta-container">
+      <ButtonCta />
+      <p className="left-column__cta-text">To See A Preview</p>
+    </div>
+  </div>
+);
+
+export default LeftColumn;
---
+++ src/layout/RightColumn.js
@@ -0,0 +1,9 @@
+import Nav from "../components/Nav";
+
+const RightColumn = () => (
+  <div className="right-column">
+    <Nav />
+  </div>
+);
+
+export default RightColumn;
