129d809734472fe8d4df05ef59c36c6f281685aa
Sandro Bochorishvili <Sanideth@gmail.com>
2021-03-29 01:04:17 +0400

    added image for the markup

--- src/layout/RightColumn.js
+++ src/layout/RightColumn.js
@@ -1,8 +1,16 @@
 import Nav from "../components/Nav";
+import Image from "../img/illustration-devices.svg";
 
 const RightColumn = () => (
   <div className="right-column">
     <Nav />
+    <div className="right-column__image-container">
+      <img
+        src={Image}
+        alt="Illustration Devices"
+        className="right-column__image"
+      />
+    </div>
   </div>
 );
 
