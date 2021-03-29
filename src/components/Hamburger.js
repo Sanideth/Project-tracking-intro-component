f4bb3f2edfa934990fbbf9e6d313e21ade388f77
Sandro Bochorishvili <Sanideth@gmail.com>
2021-03-29 15:52:28 +0400

    added hamburger component

---
+++ src/components/Hamburger.js
@@ -0,0 +1,10 @@
+import Open from "../img/icon-hamburger.svg";
+import Close from "../img/icon-close.svg";
+
+const Hamburger = () => (
+  <div className="hamburger-container">
+    <img src={Open} alt="Hamburger open icon" className="hamburger-open" />
+  </div>
+);
+
+export default Hamburger;
