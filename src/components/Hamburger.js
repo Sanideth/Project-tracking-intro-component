b9f94aac4fb8fbc92bf2ec451e3e1e2e9e26586b
Sandro Bochorishvili <Sanideth@gmail.com>
2021-03-29 17:19:52 +0400

    fixed hamburger style and transition issues

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,12 +1,20 @@
 import Open from "../img/icon-hamburger.svg";
 import Close from "../img/icon-close.svg";
 
-const Hamburger = () => (
-  <div className="hamburger-container">
-    <button className="btn btn-hamburger">
-      <img src={Open} alt="Hamburger open icon" className="hamburger-open" />
-    </button>
-  </div>
-);
+const Hamburger = (props) => {
+  const renderImage = props.isHamburgerOpen ? Close : Open;
+  console.log(props.isHamburgerOpen);
+  return (
+    <div className="hamburger-container">
+      <button className="btn btn-hamburger" onClick={props.toggleHamburger}>
+        <img
+          src={renderImage}
+          alt="Hamburger open icon"
+          className="hamburger-open"
+        />
+      </button>
+    </div>
+  );
+};
 
 export default Hamburger;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,29 @@
-import Hamberger from "./Hamburger";
+import { useState } from "react";
+import Hamburger from "./Hamburger";
 
 const mockArr = ["Product", "Features", "Pricing", "", "Login"];
 
 const Nav = () => {
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setIsHamburgerOpen(!isHamburgerOpen);
+  };
+  const styleNav = isHamburgerOpen ? "nav-mobile" : "";
   const renderElements = mockArr.map((element) => (
     <li className="nav__list-item">
       <a href="https://github.com/Sanideth" className="nav__list-link">
-        {element !== "" ? element : "\u2022"}
+        {element}
       </a>
     </li>
   ));
   return (
     <nav className="nav">
-      <ul className="nav__list">{renderElements}</ul>
-      <Hamberger />
+      <ul className={`nav__list ${styleNav}`}>{renderElements}</ul>
+      <Hamburger
+        isHamburgerOpen={isHamburgerOpen}
+        toggleHamburger={toggleHamburger}
+      />
     </nav>
   );
 };
